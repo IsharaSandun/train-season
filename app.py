b4676b46c9530bb7d9d0c71b401bffdbdf24f8fd
Ishara <isharasandun31@gmail.com>
2018-08-13 20:25:10 +0530

    breaking starter html

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,url_for,redirect,session
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('starter.html')
 
 
 if __name__ == '__main__':
