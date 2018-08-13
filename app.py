2a3f3ae4807d0ba082ee8f9cc68f76432c53e1b1
Ishara <isharasandun31@gmail.com>
2018-08-13 23:52:36 +0530

    ui change to top bar nav

--- app.py
+++ app.py
@@ -4,8 +4,17 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template('starter.html')
+
+
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
+@app.route('/register/')
+def register():
+    return render_template('register.html')
 
 
 if __name__ == '__main__':
