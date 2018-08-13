8d1e729cbd2346110d29f0f47a50114093fa0d0e
Ishara <isharasandun31@gmail.com>
2018-08-14 00:10:14 +0530

    update error pages

--- app.py
+++ app.py
@@ -19,11 +19,11 @@ def register():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('500.html')
+    return render_template('404.html', error=e)
 
 @app.errorhandler(500)
 def page_not_found(e):
-    return render_template('404.html')
+    return render_template('500.html', error=e)
 
 
 if __name__ == '__main__':
