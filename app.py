6633a3f2071840d93adea5049d42cd176e6bf55a
Ishara <isharasandun31@gmail.com>
2018-08-21 14:10:34 +0530

    ui for season add, ui season, and few othere fixes

--- app.py
+++ app.py
@@ -17,6 +17,22 @@ def register():
     return render_template('register.html')
 
 
+@app.route('/user/<string:page>/<string:page2>/')
+@app.route('/user/<string:page>/')
+def userFunctions(page,page2=""):
+
+    if (page2 == "") :
+        if (page == 'season'):
+            return render_template('season.html')
+        elif(page == 'profile'):
+            return render_template('profile.html')
+    else :
+        if (page == 'season' and page2 =='add'):
+            return render_template('season-new.html')
+
+    return render_template('404.html')
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html', error=e)
